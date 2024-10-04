275dbc754f1c26dc54929c8f25f6398b9ab07833
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2024-10-04 17:06:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,31 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+  let n = arr.length;
+
+  let pq = [];
   
+    for (let i = 0; i < n; i++) {
+        pq.push(arr[i]);
+    }    
+          
+    pq.sort(function(a,b){return a-b;});
+	let res = 0;
+
+	while (pq.length > 1) {
+
+	    let first = pq.shift();
+        let second = pq.shift();
+  
+            // Connect the ropes: update result
+            // and insert the new rope to pq
+        res += first + second;
+        pq.push(first + second);
+        pq.sort(function(a,b){return a-b;});
+        }
+  
+        return res;
+	
 }
 
 module.exports=mincost;
